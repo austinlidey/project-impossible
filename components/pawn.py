b05950f6c2c85605082cb1adf29f597a85676f96
Chaos <austin.lidey@gmail.com>
2023-11-18 13:53:05 -0800

    Add Health system and Getter to pawn

--- components/pawn.py
+++ components/pawn.py
@@ -2,10 +2,16 @@
 
 Property of Austin Lidey / Copyright 2023
 """
+from systems.health import Health
 
 
 class Pawn:
     """Class representing single pawn."""
 
     def __init__(self):
-        pass
+        self._health = Health()
+
+    @property
+    def health(self):
+        """Get Health system for this pawn."""
+        return self._health
